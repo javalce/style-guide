b5035e90184d08e2b2e6843cc004555f72d73116
javalce <javalce29@gmail.com>
2024-07-07 14:50:34 +0200

    build: add project configuration files and dependencies

---
+++ .releaserc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        // eslint-disable-next-line no-template-curly-in-string -- required by `semantic-release`.
+        message: "release: ${nextRelease.version}",
+      },
+    ],
+  ],
+};

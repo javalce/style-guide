bcf4e447212e99c84b934ef47610598243ac80b6
javalce <javalce29@gmail.com>
2024-07-10 18:16:58 +0200

    fix: update ESLint configuration for React and JSX-A11y rules

--- eslint/react.js
+++ eslint/react.js
@@ -5,8 +5,8 @@ module.exports = {
     'plugin:jsx-a11y/recommended',
     'plugin:import/react',
     'prettier',
-    require.resolve('../rules/react'),
-    require.resolve('../rules/jsx-a11y'),
+    require.resolve('./rules/react'),
+    require.resolve('./rules/jsx-a11y'),
   ],
   settings: {
     react: {
